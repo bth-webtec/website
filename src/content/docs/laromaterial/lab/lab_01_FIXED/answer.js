edad2e5060385af573c7062a2cb1002221e33e46
Mikael Roos <mikael.t.h.roos@gmail.com>
2025-04-29 10:03:59 +0200

    Spelling

--- src/content/docs/laromaterial/lab/lab_01_FIXED/answer.js
+++ src/content/docs/laromaterial/lab/lab_01_FIXED/answer.js
@@ -1,6 +1,7 @@
 /**
  * This file contains the functions you shall implement to solve the lab.
- * 
+ * Implement them one by one and check if they pass all tests.
+ *
  * Execute the lab in the browser through lab.html and review the output in the 
  * console, or execute the lab through node using `node lab` in the terminal.
  */
